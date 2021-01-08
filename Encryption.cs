860aca5b54b83fcefd8fe1d88dc6778239233898
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-01-08 14:43:15 +0100

    alt

---
+++ Encryption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SymmertriskKryptering
+{
+    public class Encryption
+    {
+        public SymmetricAlgorithm symmetricAlgorithm;
+        
+        public void GenerateSymmetricAlg(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    symmetricAlgorithm = DES.Create();
+                    break;
+                case 2:
+                    symmetricAlgorithm = TripleDES.Create();
+                    break;     
+                case 3:
+                    symmetricAlgorithm = Aes.Create();
+                    break;
+                default:
+                    break;
+            }
+            symmetricAlgorithm.GenerateKey();
+            symmetricAlgorithm.GenerateIV();
+        }
+
+        public byte[] Encrypt(byte[] dataToEncrypt)
+        {           
+                symmetricAlgorithm.Mode = CipherMode.CBC;
+                symmetricAlgorithm.Padding = PaddingMode.PKCS7;
+              
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, symmetricAlgorithm.CreateEncryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+        }
+        public byte[] Decrypt(byte[] dataToDecrypt)
+        {          
+                symmetricAlgorithm.Mode = CipherMode.CBC;
+                symmetricAlgorithm.Padding = PaddingMode.PKCS7;
+            
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, symmetricAlgorithm.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmertriskKryptering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                Encryption encryption = new Encryption();
+                Console.WriteLine("Choose:");
+                Console.WriteLine("1) Encrypt");
+                Console.WriteLine("2) Decrypt");
+                int chosen1 = int.Parse(Console.ReadLine());
+                if (chosen1 == 1)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Choose way to encrypt:");
+                    Console.WriteLine("1) DES");
+                    Console.WriteLine("2) TripleDES");
+                    Console.WriteLine("3) Aes");
+                    int chosen2 = int.Parse(Console.ReadLine());
+                    encryption.GenerateSymmetricAlg(chosen2);
+                    Console.Clear();
+                    Console.WriteLine("Write text to encrypt");
+                    string text = Console.ReadLine();
+
+                    Console.Clear();
+                    Console.WriteLine("Plaintext: " + text);
+                    Console.WriteLine("Encrypted: " + Convert.ToBase64String(encryption.Encrypt(Encoding.ASCII.GetBytes(text))));
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Choose way to decrypt:");
+                    Console.WriteLine("1) DES");
+                    Console.WriteLine("2) TripleDES");
+                    Console.WriteLine("3) Aes");
+                    int chosen3 = int.Parse(Console.ReadLine());
+                    encryption.GenerateSymmetricAlg(chosen3);
+                    Console.WriteLine("Write encrypted text");
+                    string text = Console.ReadLine();
+
+
+                    Console.WriteLine("Decrypted : " + Convert.ToBase64String(encryption.Decrypt(Encoding.ASCII.GetBytes(text))));
+
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
